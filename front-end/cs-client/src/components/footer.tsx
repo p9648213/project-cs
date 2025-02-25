224936af5189845709498b0aa6ea6892c1bb38ee
p9648213 <p9648213750912@gmail.com>
2025-02-25 21:46:42 +0700

    setup shadcn

--- front-end/cs-client/src/components/footer.tsx
+++ front-end/cs-client/src/components/footer.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useState } from "react";
+import { Button } from "./ui/button";
 
 const Footer = () => {
   const [text, setText] = useState("Hello");
 
   return (
     <div>
-      <span>{text} Footer</span>
-      <button onClick={() => setText("Test")}></button>
+      <span className="text-blue-500">{text} Footer</span>
+      <Button variant="secondary" onClick={() => setText("test")}>
+        Set text
+      </Button>
     </div>
   );
 };
